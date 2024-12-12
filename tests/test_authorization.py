daa7af415bc6b02546a8487e890861b59affe583
lexaeliseev <lexaeliseev@gmail.com>
2024-12-12 21:35:07 +0300

    fix title authorization

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -3,7 +3,7 @@ from pages.authorization_page import authorization
 
 
 def test_authorization():
-    authorization.open_authorization_page()
+    authorization.open_page()
     authorization.fill_login(os.getenv('LOGIN'))
     authorization.fill_password(os.getenv('PASSWORD'))
     authorization.click_submit()
