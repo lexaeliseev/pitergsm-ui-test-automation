941027282dac9740ef3c4ceca24e9d95e7fc17ec
lexaeliseev <lexaeliseev@gmail.com>
2024-12-12 19:38:49 +0300

    update test

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,16 +1,10 @@
+import os
 from pages.authorization_page import authorization
 
 
 def test_authorization():
     authorization.open_authorization_page()
-    authorization.fill_login('eliseevqa')
-    authorization.fill_password('SpamSpamSpam')
+    authorization.fill_login(os.getenv('LOGIN'))
+    authorization.fill_password(os.getenv('PASSWORD'))
     authorization.click_submit()
     authorization.assert_success_authorization()
-
-    # browser.open('/')
-    # browser.element('//a[@data-pop="pop-login" and contains(text(), "Вход / Регистрация")]').click()
-    # browser.element('[name="USER_LOGIN"]').type('eliseevqa')
-    # browser.element('[name="USER_PASSWORD"]').type('SpamSpamSpam')
-    # browser.element('[name="Login"]').click()
-    # browser.element('//a[@data-pop="pop-login" and contains(text(), "Личный кабинет")]').should(be.visible)
