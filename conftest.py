a7ba21c115ff8c9b0c8ab153c7497d8740c71463
lexaeliseev <lexaeliseev@gmail.com>
2024-12-12 21:34:39 +0300

    dotenv

--- conftest.py
+++ conftest.py
@@ -1,10 +1,17 @@
-import pytest
 from selene import browser
+import os
+from dotenv import load_dotenv
+import pytest
 
 
 @pytest.fixture(scope='session', autouse=True)
-def browser_settings():
-    browser.config.base_url = 'https://pitergsm.ru'
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='session', autouse=True)
+def browser_settings(load_env):
+    browser.config.base_url = os.getenv('URL')
     browser.config.window_width = '1920'
     browser.config.window_height = '1080'
     yield
