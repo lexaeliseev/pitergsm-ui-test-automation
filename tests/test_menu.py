a4cfd562ed305beea8c773b9e68f9fcaed51b29a
lexaeliseev <lexaeliseev@gmail.com>
2024-12-13 14:07:41 +0300

    update test_menu

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,15 +1,9 @@
 import pytest
-
 from pages.main_page import main_menu
-
-
-def test_menu_item_count():
-    main_menu.open_page()
-    main_menu.assert_menu_item_count(14)
 
 
 @pytest.mark.parametrize('value, id', main_menu.get_menu_items(),
                          ids=[item[1] for item in main_menu.get_menu_items()])
-def test_visible_menu_icon(value, id):
+def test_menu_item_click_and_title_validation(value, id):
     main_menu.open_page()
-    main_menu.menu_visible_menu_icon(value)
+    main_menu.menu_item_click_and_title_validation(value)
