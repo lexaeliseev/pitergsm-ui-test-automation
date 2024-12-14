d14d365b6282cbe5a891d6ae4e3bd8153f2f5d14
lexaeliseev <lexaeliseev@gmail.com>
2024-12-14 15:01:12 +0300

    update

--- conftest.py
+++ conftest.py
@@ -87,7 +87,6 @@ def setup_browser(request):
     browser.config.window_height = 1080
     browser.config.window_width = 1920
     browser.config.base_url = os.getenv('URL')
-    # browser.config.base_url = 'https://pitergsm.ru'
 
     yield
 
