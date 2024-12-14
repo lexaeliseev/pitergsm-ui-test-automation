eb6603591bc56b4b9d47f9d4a530d39a290652e5
lexaeliseev <lexaeliseev@gmail.com>
2024-12-14 22:02:50 +0300

    update add logs

--- utils/attach.py
+++ utils/attach.py
@@ -7,10 +7,6 @@ def add_screenshot(browser):
     allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
 
-# def add_logs(browser):
-#     log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
-#     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
-
 def add_logs(browser):
     log_types = ['browser', 'driver']
     logs = []
