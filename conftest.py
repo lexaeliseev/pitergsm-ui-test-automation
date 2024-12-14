819ab1fd0c222ba84188afeaf33553b1327e3f92
lexaeliseev <lexaeliseev@gmail.com>
2024-12-14 13:13:47 +0300

    driver_options.page_load_strategy = 'eager'

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ from selene import browser
 import os
 from dotenv import load_dotenv
 import pytest
+from selenium import webdriver
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -11,6 +12,10 @@ def load_env():
 
 @pytest.fixture(scope='session', autouse=True)
 def browser_settings(load_env):
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+
     browser.config.base_url = os.getenv('URL')
     browser.config.window_width = '1920'
     browser.config.window_height = '1080'
